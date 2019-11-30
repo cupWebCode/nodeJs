336f102f335372aac3c7dd92b6137146a3939e19
Ihor Voronovskyi <Ihor_Voronovskyi@epam.com>
2019-12-01 00:34:52 +0100

    task_1 - fixed err_stream_write_after_end

--- services/logger.js
+++ services/logger.js
@@ -1,22 +1,21 @@
 import EventEmitter from 'events';
 
-const _eventEmitter = new EventEmitter();
-
 const _logHandler = (e) => {
   if (e instanceof Error) {
     console.log(`======== Error has occurred ======== \ncode - ${e.code} \nmessage - ${e.message} \n====================================`);
   }
 }
 
-export class ErrorLogger {
+export class ErrorLogger extends EventEmitter {
   constructor() {
+    super();
   }
 
   log(eventName, error) {
-    _eventEmitter.emit(eventName, error);
+    this.emit(eventName, error);
   }
 
   subscribe(eventName) {
-    _eventEmitter.on(eventName, _logHandler);
+    this.on(eventName, _logHandler);
   }
 }
